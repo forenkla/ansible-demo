2024ec4f18cba7483c8bc29189ca1e2d206d1857
forenkla <46075719+forenkla@users.noreply.github.com>
2021-12-01 05:43:56 +0100

    All prerequisists for running Controller setup

---
+++ rhel_ws/pre_setup.sh
@@ -0,0 +1,15 @@
+# the Base URI of our automation controller cluster node
+export CONTROLLER_HOST=https://autoctl1.<GUID>.<SANDBOXID>.opentlc.com
+
+# the user name
+export CONTROLLER_USERNAME=admin
+
+# and the password
+export CONTROLLER_PASSWORD='VERY_SECRET_PASSWORD'
+
+# do not verify the SSL certificate, in production, you will use proper SSL certificates and not need this option or set it to True
+export CONTROLLER_VERIFY_SSL=false
+
+# install collection for automation Ansible Controller
+ansible-galaxy collection install awx.awx
+ansible-galaxy collection install community.general
